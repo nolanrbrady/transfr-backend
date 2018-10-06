dccf37cc7bb2e9e3bb37306cd33a8a4b9399350f
Nolan Brady <nolanrbrady@gmail.com>
2018-10-06 17:55:20 +0200

    Add in JWT logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ApplicationRecord
   has_many :transactions
   has_many :inventories
   has_many :coupons
+
+  def generate_jwt
+    JWT.encode({ id: id,
+      exp: 60.days.from_now.to_i },
+      Rails.application.secrets.secret_key_base)
+  end
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
