b5841ad5fad70af840f5ea3ac386ca824260a8e1
Nolan Brady <nolanrbrady@gmail.com>
2018-10-06 18:45:46 +0200

    Trying to figure out sign_up route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::API
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :encrypted_password, :wallet_address])
   end
 
   def authenticate_user
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: {sessions: :sessions},
-      path_names: {sign_in: :login}
+      path_names: {sign_in: :login, sign_up: 'create'}
       
-    resource :user, only: [:show, :update]
+    resource :user, only: [:show, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
