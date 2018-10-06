7eb533803bfe17fab56da0fc20b366cc4bf03706
Nolan Brady <nolanrbrady@gmail.com>
2018-10-06 14:14:57 +0200

    Devise routes Updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+    private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
